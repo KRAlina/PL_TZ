9e0f5b00307a53a2306db0d7cd2dbab3e104a221
KRAlina <il.kargg@gmail.com>
2024-10-17 01:15:25 +0300

    Update Main.java

--- task2/Main.java
+++ task2/Main.java
@@ -1,15 +1,35 @@
 package org.example;
 
+import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        int n = Integer.valueOf(args[0]);
-        int m = Integer.valueOf(args[1]);
-        int f = 1;
-        do {
-            System.out.print(f);
-            f = (f + (m - 2)) % n + 1;
-        } while (f != 1);
+    public static void main(String[] args) throws IOException {
+        File file1 = new File(args[0]);
+
+        File file2 = new File(args[1]);
+
+        BufferedReader reader = new BufferedReader(new FileReader(file1));
+        String[] center = (reader.readLine().split(" "));
+        double x0 = Double.valueOf(center[0]);
+        double y0 = Double.valueOf(center[1]);
+        double rS = Math.pow(Double.valueOf(reader.readLine()), 2);
+        BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+        String point;
+
+        while ((point = reader2.readLine()) != null)
+        {
+            String[] pointS = point.split(" ");
+            double x1 = Double.valueOf(pointS[0]);
+            double y1 = Double.valueOf(pointS[1]);
+            if (Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2) == rS)
+                System.out.println(0);
+            if (Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2) > rS)
+                System.out.println(2);
+            if (Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2) < rS)
+                System.out.println(1);
+        }
+
     }
 }
